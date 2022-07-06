95ed3426f97ef2d234ec6e0618a98cb44143e89c
BrainKakuza <brainditthakit@gmail.com>
2022-07-06 15:36:48 +0700

    Finish Parlindrome Number

---
+++ parlindromeNum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <sstream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::stringstream;
+
+class Solution {
+public:
+
+    bool isPalindrome(int x) {
+        stringstream ss;
+        ss << x;
+        string number = ss.str();
+
+        int length = number.length();
+        bool parlindrome = true;
+
+        int i = 0 , j = length - 1;
+
+        while(i != length - 1 && j != 0)
+        {
+            if(number[i] != number[j])
+            {
+                parlindrome = false;
+                break;
+            }
+
+            i++;
+            j--;
+        }
+
+        if(parlindrome)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+
+    }
+};
+
+int main(void)
+{
+    Solution solution;
+    cout << solution.isPalindrome(10);
+}
